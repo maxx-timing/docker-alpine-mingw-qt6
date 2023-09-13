5ee0c59e2cd1996bf438df635d1b7df2986e7bf9
Dominic <git@msrd0.de>
2023-09-13 09:28:14 +0200

    enable i686 and x86_64 target architectures

--- Dockerfile
+++ Dockerfile
@@ -8,20 +8,25 @@ WORKDIR /src
 ARG QTVER=6.5.2
 ARG QTPKGREL=0
 
-# install some necessary tools
-RUN apk add --no-cache cmake i686-mingw-w64-gcc ninja qt6-qtbase-dev=$QTVER-r$QTPKGREL
+ARG TARGET_ARCH=x86_64
 
-ADD toolchain-mingw-i686.cmake /src
+# install some necessary tools
+RUN mingw=mingw-w64-gcc; \
+	if [ "$TARGET_ARCH" == "i686" ]; then mingw="i686-$mingw"; fi; \
+	apk add --no-cache cmake "$mingw" ninja qt6-qtbase-dev=$QTVER-r$QTPKGREL
+ENV CMAKE_GENERATOR=Ninja
+
+ADD toolchain-mingw-$TARGET_ARCH.cmake /src
 
 RUN wget https://download.qt.io/official_releases/qt/${QTVER%.*}/$QTVER/submodules/qtbase-everywhere-src-$QTVER.tar.xz \
  && tar xfJ qtbase-everywhere-src-$QTVER.tar.xz \
  && cd qtbase-everywhere-src-$QTVER \
  && mkdir -p build \
- && cmake . -B build -G Ninja \
+ && cmake . -B build \
 		-DQT_HOST_PATH=/usr \
-		-DCMAKE_INSTALL_PREFIX=/usr/i686-w64-mingw32 \
+		-DCMAKE_INSTALL_PREFIX=/usr/$TARGET_ARCH-w64-mingw32 \
 		-DBUILD_SHARED_LIBS=OFF \
-		-DCMAKE_TOOLCHAIN_FILE=/src/toolchain-mingw-i686.cmake \
+		-DCMAKE_TOOLCHAIN_FILE=/src/toolchain-mingw-$TARGET_ARCH.cmake \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DINPUT_optimize_size=yes \
 		# qt modules to build \
@@ -35,4 +40,5 @@ RUN wget https://download.qt.io/official_releases/qt/${QTVER%.*}/$QTVER/submodul
 		-DQT_FEATURE_regularexpression=ON \
 		-DQT_FEATURE_system_pcre2=OFF \
  && ninja -C build install \
- && rm -rf build
+ && cd .. \
+ && rm -rf qtbase-everywhere-src-$QTVER
---
+++ toolchain-mingw-x86_64.cmake
@@ -0,0 +1,44 @@
+# https://aur.archlinux.org/cgit/aur.git/tree/toolchain-mingw.cmake?h=mingw-w64-cmake
+
+set (CMAKE_SYSTEM_NAME Windows)
+set (CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# specify the cross compiler
+set (CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set (CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
+# where is the target environment
+set (CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+# search for programs in the build host directories
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Make sure Qt can be detected by CMake
+set (QT_BINARY_DIR /usr/x86_64-w64-mingw32/bin /usr/bin)
+set (QT_INCLUDE_DIRS_NO_SYSTEM ON)
+set (QT_HOST_PATH "/usr" CACHE PATH "host path for Qt")
+
+# set the resource compiler (RHBZ #652435)
+set (CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set (CMAKE_MC_COMPILER x86_64-w64-mingw32-windmc)
+
+# override boost thread component suffix as mingw-w64-boost is compiled with threadapi=win32
+set (Boost_THREADAPI win32)
+
+# These are needed for compiling lapack (RHBZ #753906)
+set (CMAKE_Fortran_COMPILER x86_64-w64-mingw32-gfortran)
+set (CMAKE_AR:FILEPATH x86_64-w64-mingw32-ar)
+set (CMAKE_RANLIB:FILEPATH x86_64-w64-mingw32-ranlib)
+
+# https://aur.archlinux.org/cgit/aur.git/tree/toolchain-mingw-static.cmake?h=mingw-w64-cmake-static
+
+# prefer static libraries
+# note: It is of no use to set the real variable CMAKE_FIND_LIBRARY_SUFFIXES here because it gets overridden by CMake
+#       after loading the toolchain file. The project needs to make the actual override if enforcing static libraries
+#       is required.
+set(CMAKE_FIND_LIBRARY_SUFFIXES_OVERRIDE ".a;.lib")
+set(CMAKE_EXE_LINKER_FLAGS "$ENV{LDFLAGS} -static -static-libgcc -static-libstdc++" CACHE STRING "linker flags for static builds" FORCE)
